868d4f38c44e570fb508e5fc85c3ed602e5b7c59
Sangil Lee <salee@c02x44s2jg5j.redventures.net>
2022-06-13 15:49:19 -0400

    initial basic web server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+)
+
+func main() {
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello")
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
