0d4d34bf9f6debddf79a89b1e2f013ce8d927367
jbachour <42789704+jbachour@users.noreply.github.com>
2022-06-14 15:46:59 -0400

    home + search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Random from './pages/Random';
 function App() {
   return (
 
     <>
     <Navbar />
-    <Home />
+    <Random />
     </>
   );
 }
