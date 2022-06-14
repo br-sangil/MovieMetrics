2e34cd264cbcbf045693d0d4f7c96c24c4be62d2
jbachour <42789704+jbachour@users.noreply.github.com>
2022-06-14 11:18:10 -0400

    added pages skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    //should redirect to home page when clicked
+    <h1 className="p-8 text-5xl font-bold font-sans text-red-600 hover:text-red-500"> 
+    Movie Metrics 
+  </h1>
+
   );
 }
 
