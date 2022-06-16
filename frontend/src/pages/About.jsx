6c3abe613dedb53242d59309dd4e12a7e08f5bf3
jbachour <42789704+jbachour@users.noreply.github.com>
2022-06-16 09:11:46 -0400

    working on about

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className="bg-movie-posters flex h-screen justify-center items-center">
-        
+    <div className="flex flex-col h-screen">
+      
+      <div className="flex flex-row">
+          <div className="bg-red-600 w-1/2 font-sans text-white p-4 font-bold">WELCOME TO MOVIEMETRICS
+          </div>
+          <img className="w-1/2" src="/movies.jpeg" alt="logo" />
+      </div>
+
+    <div className="flex h-96">
+        <div className="bg-red-300 w-1/3 text-6xl font-sans text-white p-4 font-bold">About us</div>
+        <div className="bg-slate-300 w-1/3">4</div>
+        <div className="bg-red-500 w-1/3">5</div>
+    </div>
+
+    <div className="flex">
+    <img className="w-1/2" src="/movies3.jpeg" alt="logo" />
+        <div className="bg-black w-1/2"></div>
+    </div>
 
     </div>
   )
--- frontend/src/pages/Random.jsx
+++ frontend/src/pages/Random.jsx
@@ -14,17 +14,22 @@ export default function Home() {
     
     if (!movie || movie === undefined || movie.length === 0) {
       return (
-        <a href="/random">
-        <img className="w-40" src="/movie_random.png" alt="logo" />
-      </a>
-      )
+        <div>
+          Loading
+        </div>
+      );
     }
 
     return (
-      <div>
-       
-        <MovieInfo movie={movie} />
+      <div className="bg-movie-posters flex h-screen justify-center items-center">
+          <div className="bg-slate-100 w-4/5 h-3/4 drop-shadow-2xl rounded-lg">
+            <img className="pt-4 m-auto" src="/movie_random.png" alt="logo" />
+            <MovieInfo movie={movie} />
+          </div>
+
       </div>
+
+       
       
     );
 }
